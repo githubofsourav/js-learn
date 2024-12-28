9dc7fab96ca6f1799fa69be45e2d8d69b52817e5
githubofsourav <souravdas1301@gmail.com>
2024-12-28 14:40:11 +0530

    Add 02_dataTypes.js to demonstrate and understand data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Sourav");
+
+let name = "sourav";
+let age = 24;
+let isLoggedIn = false;
+
+// number => 2^53 (range)
+// bigInt => to store very large numbers
+// string => "", text inside quotes
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "sourav"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
